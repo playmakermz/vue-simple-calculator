3ed2980ed587f9034ab3d5914ffc0a3d678f5c71
3.sahabat.002275.rq1 <3.sahabat.002275.rq1@gmail.com>
2025-06-22 03:26:47 +0000

    new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,62 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
+const myForm = ref() // Ini adalh stance 
 
-const count = ref(0)
+function mySys(nilai){
+  try {
+    let abc = parseInt(myForm.value)
+    myForm.value += nilai 
+  } catch (e) {
+    myForm.value = ''
+  }
+ 
+}
+
+function myClear(){
+  myForm.value = ''
+}
+
+function Calculate(){
+  try {
+    myForm.value = eval(myForm.value)
+  } catch (e) {
+    myForm.value = 'Error'
+  }
+}
+
+
 </script>
 
 <template>
  
  <div class="calculator">
-        <input type="text" id="display" readonly>
-        <div class="buttons">
-            <button data-value="7">7</button>
-            <button data-value="8">8</button>
-            <button data-value="9">9</button>
-            <button data-value="/">/</button>
-            <button data-value="4">4</button>
-            <button data-value="5">5</button>
-            <button data-value="6">6</button>
-            <button data-value="*">*</button>
-            <button data-value="1">1</button>
-            <button data-value="2">2</button>
-            <button data-value="3">3</button>
-            <button data-value="-">-</button>
-            <button data-value="0">0</button>
-            <button data-value=".">.</button>
-            <button data-value="=">=</button>
-            <button data-value="+">+</button>
-        </div>
+   <form @submit.prevent="">
+    <input v-model="myForm" required placeholder="0">
+    <div class="buttons">
+      <button @click="mySys('7')">7</button>
+      <button @click="mySys('8')">8</button>
+      <button @click="mySys('9')">9</button>
+      <button @click="mySys('/')">/</button>
+      <button @click="mySys('4')">4</button>
+      <button @click="mySys('5')">5</button>
+      <button @click="mySys('6')">6</button>
+      <button @click="mySys('*')">*</button>
+      <button @click="mySys('1')">1</button>
+      <button @click="mySys('2')">2</button>
+      <button @click="mySys('3')">3</button>
+      <button @click="mySys('-')">-</button>
+      <button @click="mySys('0')">0</button>
+      <button @click="mySys('.')">.</button>
+      <button @click="Calculate()">=</button>
+      <button @click="mySys('+')">+</button>
+
+      <button @click="myClear()">Clear</button>
+      <button @click="mySys('.')">Delete</button>
+
     </div>
+   </form>
+  </div>
 
 </template>
 
